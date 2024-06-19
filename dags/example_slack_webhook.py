8c50f51c679111a5742960380b5df914894e5fb6
ifntsh <99662127+ifntsh@users.noreply.github.com>
2024-06-19 13:56:26 +0900

    Update example_slack_webhook.py

--- dags/example_slack_webhook.py
+++ dags/example_slack_webhook.py
@@ -1,27 +1,9 @@
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-from __future__ import annotations
-
-import os
-from datetime import datetime, timedelta
-
-from airflow.models.dag import DAG
+from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
+
+import os
+from datetime import datetime
 
 ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
 DAG_ID = "slack_webhook_example_dag"
@@ -30,7 +12,7 @@ IMAGE_URL = "https://raw.githubusercontent.com/apache/airflow/main/airflow/www/s
 
 def log_current_time(**context):
     current_time = datetime.now().time()
-    message = f"Current time is {current_time}"
+    message = f":slack: UTC time is {current_time}"
     print(message)
     return message
 
@@ -58,6 +40,3 @@ with DAG(
     # [END slack_webhook_operator_text_howto_guide]
 
     generate_log_message >> slack_webhook_operator_text
-
-if __name__ == "__main__":
-    dag.cli()
