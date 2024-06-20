804dae13a675ba0f0156369ac2dbd00499073aa1
ifntsh <99662127+ifntsh@users.noreply.github.com>
2024-06-20 15:15:31 +0900

    Update example_slack_webhook.py

--- dags/example_slack_webhook.py
+++ dags/example_slack_webhook.py
@@ -6,15 +6,32 @@ import os
 from datetime import datetime
 
 ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
-DAG_ID = "slack_webhook_example_dag"
+DAG_ID = "on_time_alarm"
 SLACK_WEBHOOK_CONN_ID = os.environ.get("SLACK_WEBHOOK_CONN_ID", "slack_webhook")
-IMAGE_URL = "https://raw.githubusercontent.com/apache/airflow/main/airflow/www/static/pin_100.png"
 
 def log_current_time(**context):
     current_time = datetime.now().time()
     message = f":slack: UTC time is {current_time}"
     print(message)
     return message
+
+def task_fail_slack_alert(context):
+    slack_msg = f"""
+    :red_circle: Task Failed.
+    *Task*: {context.get('task_instance').task_id}
+    *Dag*: {context.get('task_instance').dag_id}
+    *Execution Time*: {context.get('execution_date')}
+    *Log Url*: {context.get('task_instance').log_url}
+    """
+    
+    failed_alert = SlackWebhookOperator(
+        task_id='slack_failed',
+        http_conn_id=SLACK_WEBHOOK_CONN_ID,
+        message=slack_msg,
+        username='airflow',
+    )
+    
+    return failed_alert.execute(context=context)
 
 with DAG(
     dag_id=DAG_ID,
@@ -23,6 +40,9 @@ with DAG(
     max_active_runs=1,
     catchup=False,
     tags=["example"],
+    default_args={
+        'on_failure_callback': task_fail_slack_alert,
+    }
 ) as dag:
     # 로그 메시지를 생성하는 PythonOperator
     generate_log_message = PythonOperator(
